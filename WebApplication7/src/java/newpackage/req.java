eacbc92a98922744894bd353a591f74fee7634e0
ydksoysa <135942072+ydksoysa@users.noreply.github.com>
2024-05-01 16:27:01 +0530

    login page
    creating login page using html,css and jsp with servlet function

---
+++ WebApplication7/src/java/newpackage/req.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+import javax.servlet.RequestDispatcher;
+
+/**
+ *
+ * @author dilshara
+ */
+class req {
+
+    static String getParameter(String userName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static RequestDispatcher getRequestDispatcher(String welcome) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ WebApplication7/src/java/newpackage/res.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author dilshara
+ */
+class res {
+
+    static void setContentType(String texthtml) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static PrintWriter getWriter() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
